29411c660d792cee35b70836eac8225a7beb9749
Candra Saputra <candrasaputra@live.com>
2024-07-21 00:18:13 +0700

    feat: register and login

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Validator;
+use Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'ada kesalahan',
+                'data' => $validator->errors()
+            ]);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
+        $success['name'] = $user->name;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Sukses register',
+            'data' => $success
+        ]);
+    }
+
+    public function login(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|min:4|max:100',
+            'password' => 'required|min:8|max:100'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Invalid input',
+                'validations' => $validator->errors()
+            ]);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+
+            $success['token'] = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'data' => $success
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'username atau password salah'
+            ]);
+        };
+    }
+}
